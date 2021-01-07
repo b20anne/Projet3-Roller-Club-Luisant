1997af1a78dd6d336971d757e302cca0e2f138da
FlorentLem <florentlempro@gmail.com>
2021-01-07 10:07:03 +0100

    input done, i need to style it now

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -5,7 +5,13 @@ import imageOne from "./assets/register-picture.jpeg";
 import imageTwo from "./assets/roller.png";
 
 const Register = () => {
-const [email, setEmail] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   return (
     <div className="registerPageContainer">
@@ -31,7 +37,7 @@ const [email, setEmail] = useState("");
           </div>
         </div>
         <div className="registerRight">
-          <div>
+          <div className="formContainer">
             <div>
               <h1>Inscription</h1>
               <p>Créer votre compte pour accéder à nos offres</p>
@@ -40,11 +46,101 @@ const [email, setEmail] = useState("");
                 créer votre compte.
               </p>
             </div>
-            <form>
-              <div>
-                <label htmlFor="email">Email</label>
-                <input id="email" name="email" type="text" value={email} />
+            <form className="registerForm">
+              <div className="inputField">
+                <div className="inputRegister">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    id="email"
+                    name="email"
+                    type="text"
+                    value={email}
+                    onChange={(event) => {
+                      setEmail(event.target.value);
+                    }}
+                  />
+                </div>
+                <div className="inputRegister">
+                  <label htmlFor="password">Mot de passe</label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    value={password}
+                    onChange={(event) => {
+                      setPassword(event.target.value);
+                    }}
+                  />
+                </div>
+                <div className="inputRegister">
+                  <label htmlFor="confirmedPassword">
+                    Confirmé mot de passe
+                  </label>
+                  <input
+                    id="confirmedPassword"
+                    name="confirmedPassword"
+                    type="password"
+                    value={confirmedPassword}
+                    onChange={(event) => {
+                      setConfirmedPassword(event.target.value);
+                    }}
+                  />
+                </div>
+                <div className="inputRegister">
+                  <label htmlFor="firsName">Prénom</label>
+                  <input
+                    id="firsName"
+                    name="firsName"
+                    type="text"
+                    value={firstName}
+                    onChange={(event) => {
+                      setFirstName(event.target.value);
+                    }}
+                  />
+                </div>
+                <div className="inputRegister">
+                  <label htmlFor="lastName">Nom</label>
+                  <input
+                    id="lastName"
+                    name="lastName"
+                    type="text"
+                    value={lastName}
+                    onChange={(event) => {
+                      setLastName(event.target.value);
+                    }}
+                  />
+                </div>
+                <div className="inputRegister">
+                  <label htmlFor="age">Age</label>
+                  <input
+                    id="age"
+                    name="age"
+                    type="text"
+                    value={age}
+                    onChange={(event) => {
+                      setAge(event.target.value);
+                    }}
+                  />
+                </div>
+                <div className="inputRegister">
+                  <label htmlFor="phoneNumber">Numéro de téléphone</label>
+                  <input
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    type="text"
+                    value={phoneNumber}
+                    onChange={(event) => {
+                      setPhoneNumber(event.target.value);
+                    }}
+                  />
+                </div>
               </div>
+              <div>
+                <input type="checkbox"/>
+                <span>J'accepte les conditions d'utilisations</span>
+              </div>
+              <button>Inscription</button>
+              <p>Déjà inscrit ? Connectez vous</p>
             </form>
           </div>
         </div>
