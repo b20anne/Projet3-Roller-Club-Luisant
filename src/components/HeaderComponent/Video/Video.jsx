b414b19401dafe82b386d9f78670f4e553ce0a30
LafondMael <98mael@laposte.net>
2021-01-12 11:32:28 +0100

    Header 2.1

--- src/components/HeaderComponent/Video/Video.jsx
+++ src/components/HeaderComponent/Video/Video.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Player, ControlBar, PlayToggle } from "video-react";
+import { Player, ControlBar, PlayToggle, BigPlayButton } from "video-react";
 import "../../../../node_modules/video-react/styles/scss/video-react.scss";
 import "./Video.scss";
 
@@ -8,9 +8,11 @@ const HeaderVid = "https://media.w3.org/2010/05/sintel/trailer_hd.mp4";
 const RollerVideo = () => {
   return (
     <Player src={HeaderVid} className="PlayVid">
-      <ControlBar disableDefaultControls>
-        <PlayToggle />
-      </ControlBar>
+      <BigPlayButton position="center">
+        <ControlBar disableDefaultControls>
+          <PlayToggle />
+        </ControlBar>
+      </BigPlayButton>
     </Player>
   );
 };
