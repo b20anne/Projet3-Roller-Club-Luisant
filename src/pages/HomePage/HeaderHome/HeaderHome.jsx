713831404c028ed2dd621f951cd91b04771c8cb7
LafondMael <98mael@laposte.net>
2021-01-12 15:21:41 +0100

    Linked with register and login

--- src/pages/HomePage/HeaderHome/HeaderHome.jsx
+++ src/pages/HomePage/HeaderHome/HeaderHome.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { BiBasket } from "react-icons/bi";
 import { RiCalendarEventFill } from "react-icons/ri";
 import Logo from "../../../components/HeaderComponent/Logo.png";
@@ -13,12 +14,12 @@ const HeaderHome = () => {
           <img src={Logo} alt="logo" />
         </div>
         <div className="doubleBttn">
-          <button as="input" type="button" className="connect">
-            Connexion
-          </button>
-          <button as="input" type="button" className="register">
-            Inscription
-          </button>
+          <div className="connect">
+            <Link to="/login">Connexion</Link>
+          </div>
+          <div className="register">
+            <Link to="/register">Inscription</Link>
+          </div>
         </div>
       </div>
       <div className="rightDiv">
