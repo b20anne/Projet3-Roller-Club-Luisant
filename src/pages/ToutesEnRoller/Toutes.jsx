33ef889a8387be655515b3816ac56fcf81d1b5d0
LafondMael <98mael@laposte.net>
2021-01-19 10:01:01 +0100

    Finished Toutes en Roller

--- src/pages/ToutesEnRoller/Toutes.jsx
+++ src/pages/ToutesEnRoller/Toutes.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import LogoTER from "./assets/TER.png";
 import Logo1 from "./assets/FNCIDFF.png";
 import Logo2 from "./assets/TOUTE.png";
+import R1 from "./assets/R1.jpg";
+import R2 from "./assets/R2.jpg";
+import R3 from "./assets/R3.jpg";
 import "./Toutes.scss";
 
 const Toutes = () => {
@@ -13,27 +16,40 @@ const Toutes = () => {
       </div>
       <div className="TERDesc">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut
-          bibendum orci. Phasellus quis metus convallis, tempus ligula non,
-          convallis nisl. Nulla facilisi. Nulla ultricies gravida leo, a posuere
-          mi faucibus ut. Ut eu sem eget tellus vehicula rutrum eu quis mauris.
-          Nam sit amet dolor ut odio hendrerit hendrerit in quis libero. Donec
-          finibus eu ante et tincidunt. Duis tincidunt venenatis nisl id ornare.
-          Pellentesque habitant morbi tristique senectus et netus et malesuada
-          fames ac turpis egestas. Duis at mi turpis. Duis feugiat imperdiet
-          enim, id viverra velit egestas eu. Vestibulum posuere faucibus
-          accumsan.
+          &emsp;Inspiré par le projet TouteSport de la Fédération Nationale des
+          CIDFF, Toutes en Roller met en avant la performance sportive des
+          athlètes féminines au travers des courses de Roller phares et
+          populaires.
+          <br />
+          <br />
+          &emsp;Une équipe composée de dix patineuses de haut niveau
+          représentera Toutes en Roller sur l’épreuve mythique des 24h du Mans.
+          Une manifestation rassemblant chaque année plus de 100 000
+          participants. Sa mission est de remporter la compétition dans la
+          catégorie Prestige Femme. <br />
+          <br />
+          &emsp;Les patineuses du RC Luisant porteront les couleurs de Toutes en
+          Roller Roller sur les différentes courses de 6h et elles tenteront de
+          se hisser sur la plus haute marche du podium tout au long de la
+          saison. <br />
+          <br />
+          &emsp;Pour chaque kilomètre parcouru en course par les équipes Toutes
+          en Roller, un euro sera reversé à la Fédération Nationale des CIDFF,
+          une des associations les plus renommées de France en matière d’accès
+          aux droits pour les femmes, de lutte contre les discriminations
+          sexistes et de promotion de l’égalité entre les femmes et les hommes.{" "}
+          <br />
         </p>
       </div>
       <div className="TERRoll">
         <div className="Roll1">
-          <p>Carte1</p>
+          <img src={R1} alt="Roller1" />
         </div>
         <div className="Roll2">
-          <p>Carte2</p>
+          <img src={R2} alt="Roller2" />
         </div>
         <div className="Roll3">
-          <p>Carte3</p>
+          <img src={R3} alt="Roller3" />
         </div>
       </div>
       <div className="TERLogos">
