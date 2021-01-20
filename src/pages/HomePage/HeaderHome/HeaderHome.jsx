039640140ed2a10f641048b6e5ae57508d4e2213
b20anne <b20anne@yahoo.fr>
2021-01-20 17:06:28 +0100

    correcterreurs orthographe

--- src/pages/HomePage/HeaderHome/HeaderHome.jsx
+++ src/pages/HomePage/HeaderHome/HeaderHome.jsx
@@ -28,7 +28,7 @@ const HeaderHome = () => {
         </div>
         <div className="otherButtons">
           <button as="input" type="button" className="market">
-            <p>Catalogue Produits</p>
+            <p>Catalogue des Produits</p>
             <div className="Basket">
               <BiBasket size="3em" />
             </div>
