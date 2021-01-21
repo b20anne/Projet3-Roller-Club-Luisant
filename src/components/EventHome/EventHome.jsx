8166504a2f2f2a811bf28be1f3d3ccc2e095ab05
LafondMael <98mael@laposte.net>
2021-01-21 10:59:34 +0100

    oups

--- src/components/EventHome/EventHome.jsx
+++ src/components/EventHome/EventHome.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import TER from "../../pages/ToutesEnRoller/assets/TER.png";
-import Toute from "../../pages/ToutesEnRoller/assets/TOUTE.png";
 import LinkButton from "./EventButton";
 import "./EventHome.scss";
 
@@ -9,8 +8,7 @@ const EventHome = () => {
     <div className="TERProjet">
       <div>
         <h1>Les Projets</h1>
-        <img src={TER} alt="TER" />
-        <img src={Toute} alt="Toute" />
+        <img src={TER} className="TERTitleLogo" alt="TER" />
       </div>
       <div className="TERText">
         <h3>Toutes en Roller</h3>
@@ -18,12 +16,6 @@ const EventHome = () => {
           Inspiré par le projet TouteSport de la Fédération Nationale des CDIFF,
           Toutes en Roller met en avant la performance sportive des athlètes
           féminines au travers des courses de Roller phares et populaires.
-          <br /> <br />
-          Une équipe composée de dix patineuses de haut niveau représentera
-          Toutes en Roller sur l’épreuve mythique des 24h du Mans. Une
-          manifestation rassemblant chaque année plus de 100 000 participants.
-          Sa mission est de remporter la compétition dans la catégorie Prestige
-          Femme.
         </p>
         <LinkButton />
       </div>
