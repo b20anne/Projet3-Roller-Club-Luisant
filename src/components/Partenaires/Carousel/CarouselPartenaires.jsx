bdf12b8a886af45cf721ba8dde80a570841062bc
LafondMael <98mael@laposte.net>
2021-01-25 13:46:40 +0100

    carousel partenaires

---
+++ src/components/Partenaires/Carousel/CarouselPartenaires.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import "./CarouselPartenaires.css";
+import axa from "./assets/AXA.PNG";
+import Aviva from "./assets/AVIVA.jpg";
+import Paris from "./assets/Paris.jpg";
+import CAPTN from "./assets/CAPTN.png";
+import Chapron from "./assets/Chapron.jpg";
+import Luisant from "./assets/LUISANT.png";
+import Mainvilliers from "./assets/MAINVILLIERS.png";
+
+const responsive = {
+  0: { items: 1 },
+  568: { items: 2 },
+  1024: { items: 3 },
+};
+
+const objet = [
+  <div className="item" data-value="1">
+    <img src={axa} alt="" />
+  </div>,
+  <div className="item" data-value="2">
+    <img src={Aviva} alt="" />
+  </div>,
+  <div className="item" data-value="3">
+    <img src={Paris} alt="" />
+  </div>,
+  <div className="item" data-value="4">
+    <img src={CAPTN} alt="" />
+  </div>,
+  <div className="item" data-value="5">
+    <img src={Chapron} alt="" />
+  </div>,
+  <div className="item" data-value="6">
+    <img src={Luisant} alt="" />
+  </div>,
+  <div className="item" data-value="7">
+    <img src={Mainvilliers} alt="" />
+  </div>,
+];
+
+const CarouselP = () => (
+  <div className="CarouselPartenaires">
+    <h1 className="Partenaires">Partenaires</h1>
+    <AliceCarousel
+      mouseTracking
+      items={objet}
+      responsive={responsive}
+      autoPlay
+      autoPlayInterval="2000"
+      disableDotsControls
+      disableButtonsControls
+      infinite
+    />
+  </div>
+);
+
+export default CarouselP;
