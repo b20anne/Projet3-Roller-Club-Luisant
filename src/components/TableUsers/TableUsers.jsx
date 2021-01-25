8e95970dd1b92734376e8fc2d7ba1e1bc90efd87
FlorentLem <florentlempro@gmail.com>
2021-01-25 11:46:59 +0100

    fix responsive

--- src/components/TableUsers/TableUsers.jsx
+++ src/components/TableUsers/TableUsers.jsx
@@ -8,30 +8,6 @@ const TableUsers = ({ users }) => {
       <h1>Tableaux des utilisateurs : </h1>
       <table className="rwd-table">
         <tbody>
-          <tr>
-            <th>Nom</th>
-            <th>Prénom</th>
-            <th>Status</th>
-            <th>Âge</th>
-          </tr>
-          <tr>
-            <td data-th="Name">oui</td>
-            <td data-th="Prénom">yes</td>
-            <td data-th="Status">Baby</td>
-            <td data-th="Âge">12 ans</td>
-          </tr>
-          <tr>
-            <td data-th="Name">oui</td>
-            <td data-th="Prénom">yes</td>
-            <td data-th="Status">Baby</td>
-            <td data-th="Âge">12 ans</td>
-          </tr>
-          <tr>
-            <td data-th="Name">oui</td>
-            <td data-th="Prénom">yes</td>
-            <td data-th="Status">Baby</td>
-            <td data-th="Âge">12 ans</td>
-          </tr>
           {users.map((el) => (
             <tr key={el.id}>
               <td data-th="Name">{el.lastName}</td>
