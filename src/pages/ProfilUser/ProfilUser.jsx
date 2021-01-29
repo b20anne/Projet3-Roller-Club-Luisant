4a352752e689ce4ee325e0e8aa62f1a608feaaed
Sovakiiir <valentin.broudinpro@gmail.com>
2021-01-29 09:40:13 +0100

    Maj Profile

--- src/pages/ProfilUser/ProfilUser.jsx
+++ src/pages/ProfilUser/ProfilUser.jsx
@@ -3,33 +3,68 @@ import "../ProfilUser/ProfilUser.css";
 import Logo1 from "../../components/assets/Roller.jpg";
 import { MdPhone } from "react-icons/md";
 import { MdMailOutline } from "react-icons/md";
-import { FaPen } from "react-icons/fa";
 import { MdNotInterested } from "react-icons/md";
+import { connect } from "react-redux";
+import axios from "axios";
 
-const ProfilUser = () => {
-    return (
-        <div>
-            <div className="profilCardUser">
-                <img src={Logo1} className="photoUser" />
-                <div className="descriptUser">
-                    <div className="nameUser">Jean Denis Test</div>
-                    <div className="introUser">Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis nobis similique optio. Inventore cupiditate possimus architecto quisquam ea,
-                    fuga vel ad laudantium,
-                    in neque culpa distinctio dolorum, est libero velit!</div>
-                    <div className="statusUser">Status :</div>
-                    <div className="groupBttn">
-                    <button className ="bttnDeco" alt="Déco">Déconnexion<MdNotInterested style={{fontSize: '15px'}}/></button>
-                        <button className="bttnModif" alt="Modif">Modifier<FaPen style={{fontSize: '15px'}}/></button>
+const API_URL = process.env.REACT_APP_API_URL;
+
+class ProfilUser extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          user: "",
+        };
+      }
+      componentDidMount() {
+        this.getUser();
+      }
+    
+      getUser() {
+
+        const{id}=this.props.userInfo
+        console.log(this.props)
+
+        axios
+          .get(`${API_URL}/api/profile/${id}`)
+    
+          .then((response) => response.data)
+          .then((data) => {
+            this.setState({
+              user: data,
+            });
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+       };
+            
+    render () {
+        const {user}=this.state;
+        return (
+            <div>
+                {user ? (<><div className="profilCardUser">
+                    <img src={Logo1} className="photoUser" />
+                    <div className="descriptUser">
+                        <div className="nameUser">Nom / Prénom : {user.lastName} {user.firstName}</div>
+                        <div className="statusUser">Âge : {user.age}</div>
+                        <div className="groupBttn">
+                        <button className ="bttnDeco" alt="Déco">Déconnexion<MdNotInterested style={{fontSize: '15px'}}/></button>
+                        </div>
                     </div>
                 </div>
+                <div className="contactCard">
+                    <div className="userPhone"><MdPhone style={{fontSize: '25px'}}/>Téléphone : {user.Phone_Number}</div>
+                    <div className="userEmail"><MdMailOutline style={{fontSize: '25px'}}/>Email : {user.email}</div>
+                </div></>) : (<p>loadding</p>)}
             </div>
-            <div className="contactCard">
-                <div className="userPhone"><MdPhone style={{fontSize: '25px'}}/>Téléphone</div>
-                <div className="userEmail"><MdMailOutline style={{fontSize: '25px'}}/>Email</div>
-            </div>
-        </div>
-    );
-  };
+        );
+    }
+};
   
-  export default ProfilUser;
-  
+const mapStateToProps = (state) => ({
+    userInfo: state.auth.user,
+  });
+
+  export default connect(mapStateToProps)(ProfilUser);
